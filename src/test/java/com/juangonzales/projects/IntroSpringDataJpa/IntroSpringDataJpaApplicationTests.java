e584d3b165cb2bc73ff8f1cacdad53c8e9294aef
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-06 20:39:20 -0400

    iniciando proyecto con jpa

---
+++ src/test/java/com/juangonzales/projects/IntroSpringDataJpa/IntroSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juangonzales.projects.IntroSpringDataJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
