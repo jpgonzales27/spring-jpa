956f01314771fa54479f968b34fd0747139ebe24
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-06 21:30:28 -0400

    probando conexion a BD con datasource y EntityMangerFactory

--- src/main/java/com/juangonzales/projects/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
+++ src/main/java/com/juangonzales/projects/IntroSpringDataJpa/IntroSpringDataJpaApplication.java
@@ -1,7 +1,16 @@
 package com.juangonzales.projects.IntroSpringDataJpa;
 
+import jakarta.persistence.EntityManager;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
 
 @SpringBootApplication
 public class IntroSpringDataJpaApplication {
@@ -10,4 +19,33 @@ public class IntroSpringDataJpaApplication {
 		SpringApplication.run(IntroSpringDataJpaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner validateDSCommand(DataSource dataSource){
+		return args -> {
+			System.out.println("\n Probando Conexión y DS\n");
+			Connection conn = dataSource.getConnection();
+			PreparedStatement pstm = conn.prepareStatement("select * from characters");
+			ResultSet rs = pstm.executeQuery();
+			while(rs.next()){
+				String mensaje = rs.getString("id") + " - " + rs.getString("nombre");
+				System.out.println(mensaje);
+			}
+			System.out.println("\n\n ");
+		};
+	}
+
+	@Bean
+	public CommandLineRunner validateEntityManagerCommand(EntityManager em){
+		return args -> {
+			System.out.println("\nProbando EntityManagerFactory\n");
+			List<Object[]> result = em.createNativeQuery("select * from characters").getResultList();
+
+			result.forEach(each -> {
+				String mensaje = each[0] + " - " + each[1];
+				System.out.println(mensaje);
+			});
+			System.out.println("\n\n");
+
+		};
+	}
 }
